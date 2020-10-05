b01aaa395dd630edc4daa6cea3ff2edbb16df77a
Yuri Baldan <yuribbp@gmail.com>
2020-10-05 20:43:42 -0300

    Creating project

---
+++ src/gui/ViewController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+
+	@FXML
+	private Button btTest;
+
+	@FXML
+	public void onBtTestAction() {
+		Alerts.showAlert("Alert title", null, "Hello!", AlertType.ERROR);
+	}
+}
