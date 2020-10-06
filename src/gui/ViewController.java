f560d56aefa0875b9e9a96e9d37d0addfc786481
Yuri Baldan <yuribbp@gmail.com>
2020-10-05 21:03:19 -0300

    Including SUM and Result fields and Sum button

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,17 +1,29 @@
 package gui;
 
-import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
 
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", null, "Hello!", AlertType.ERROR);
+	public void onBtSumAction() {
+		double number1 = Double.parseDouble(txtNumber1.getText());
+		double number2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + number2;
+		labelResult.setText(String.format("%.2f", sum));
 	}
 }
